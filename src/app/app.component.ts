41b90bb8a09be3f4dc4b4af13d773a92319cbb8f
phuonghoaivo <phuonghvo@yahoo.com>
2017-07-15 23:04:13 +0700

    Add selected type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,18 @@ import { FactResult } from './fact-detail/fact-detail';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
     title = 'app';
     number: number;
     fact: string;
-
+    myType: string;
+    types = [
+        { code: '1', name: 'trivia' },
+        { code: '2', name: 'math' },
+        { code: '3', name: 'date' },
+        { code: '4', name: 'year' }
+    ]
 
     /**
      * Creates an instance of AppComponent.
@@ -23,16 +30,27 @@ export class AppComponent {
      */
     constructor(private numberService: NumberService, public dialog: MdDialog) {
         this.number = 0;
+        this.myType = '1';
     }
 
     getFact(): void {
         console.log('[getFact] Number: ' + this.number);
-        this.numberService.resolveFact(this.number, 'trivia').then(response => {
+        this.numberService.resolveFact(this.number, this.getType()).then(response => {
             console.log('[getFact] Get fact successfully: ' + response);
             this.fact = response as string;
             this.openDialog('Detail:', this.fact);
         })
-            .catch(error => this.handleException)
+        .catch(error => this.handleException)
+    }
+
+    getType(): string {
+        let type = null;
+        this.types.forEach(element => {
+            if (element.code === this.myType) {
+                type = element.name;
+            }
+        });
+        return type;
     }
 
     openDialog(title: string, message: string) {
--- src/app/fact-detail/fact-detail.component.ts
+++ src/app/fact-detail/fact-detail.component.ts
@@ -7,8 +7,8 @@ import { MD_DIALOG_DATA } from '@angular/material';
     template: `
     <h2 md-dialog-title>{{data.title}}</h2>
     <md-dialog-content style="font-family: 'Roboto';">{{data.message}}</md-dialog-content>
-    <md-dialog-actions>
-      <button md-button md-dialog-close>Close</button>
+    <md-dialog-actions style="float: right;">
+      <button md-button md-dialog-close style="background-color:#c0392b; color: #fff">Close</button>
     </md-dialog-actions>`,
 })
 export class FactDetailComponent implements OnInit {
